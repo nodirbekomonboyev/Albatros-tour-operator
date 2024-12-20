bdf7579953519dbd2896dc3755356cfde03d44f7
nodirbekomonboyev <omonboevnodir77@gmail.com>
2024-12-20 19:11:23 +0500

    Initial commit

---
+++ src/main/java/com/nodirverse/albatros/AlbatrosApplication.java
@@ -0,0 +1,13 @@
+package com.nodirverse.albatros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlbatrosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlbatrosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nodirverse/albatros/AlbatrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nodirverse.albatros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlbatrosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
